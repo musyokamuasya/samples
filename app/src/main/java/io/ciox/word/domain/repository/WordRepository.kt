f21dcc9872e3a72813901f40d4d707e9b126b3e5
musyokamuasya <josephmuasya8@gmail.com>
2022-01-14 10:01:42 +0300

    Implement word repository

---
+++ app/src/main/java/io/ciox/word/domain/repository/WordRepository.kt
@@ -0,0 +1,17 @@
+package io.ciox.word.domain.repository
+
+import androidx.annotation.WorkerThread
+import io.ciox.word.domain.dao.WordDao
+import io.ciox.word.domain.entity.Word
+import kotlinx.coroutines.flow.Flow
+
+class WordRepository (private val wordDao: WordDao) {
+    val allWords: Flow<List<Word>> = wordDao.getAlphabetizedWords()
+
+
+
+    @WorkerThread
+    suspend fun insertWord(word: Word){
+        wordDao.addWord(word)
+    }
+}
